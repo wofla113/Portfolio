9a56a1cd8fe532542d9e96749d47654838980ec4
jaerim <93243053+wofla113@users.noreply.github.com>
2023-03-08 17:10:07 +0900

    modify design

--- js/nav-bar.js
+++
@@ -1,8 +0,0 @@
-const navBarList = document.querySelector(".nav-bar__list");
-
-function handleNavBar(event){
-    const clickedBtn = event.target;
-    clickedBtn.classList.add(clicked);
-}
-
-navBarList.addEventListener("click",handleNavBar);
