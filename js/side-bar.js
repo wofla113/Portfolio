8282e431552e708c65de998e9758a2613f30cbdd
jaerim <93243053+wofla113@users.noreply.github.com>
2023-03-15 13:16:43 +0900

    modify design3

--- js/side-bar.js
+++ js/side-bar.js
@@ -4,10 +4,12 @@ $(".side-bar").on({
         $(".side-bar__btn span").removeClass("hidden");
         $(".side-bar__btn").addClass("btnRow");
         $(".side-bar__btn span").addClass("btnMargin");
+        $(".side-bar__btn img").addClass("borderNone");
     },
     mouseleave: function(){
         $(this).removeClass("sideBarOpen");
         $(".side-bar__btn span").addClass("hidden");
         $(".side-bar__btn").removeClass("btnRow");
+        $(".side-bar__btn img").removeClass("borderNone");
     }
 });
